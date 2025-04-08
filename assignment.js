a7fcb53c3e4d43f1f25e2b709125a3cfa5985ab7
SpyderXIV <151566749+SpyderXIV@users.noreply.github.com>
2025-04-08 09:15:55 -0400

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,23 +1,3 @@
-/*
-Instructions:
-
-1. Create a Stack class with the following methods:
-
-    - push(item) – adds an item to the top
-
-    - pop() – removes the top item
-
-    - peek() – returns the top item without removing it
-
-2. Test your stack by:
-
-    - Pushing 2 items
-
-    - Popping one
-
-    - Printing the item on top with peek()
-*/
-
 // Starter code:
 
 class Stack {
